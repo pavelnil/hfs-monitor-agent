d5d581b8d0d981f3283ee70b2f7b265057a17cd2
pavelnil <pavelmqx@gmail.com>
2025-08-07 23:57:52 +0300

    Add files via upload

--- dist-win32/plugin.js
+++ dist-win32/plugin.js
@@ -2,6 +2,8 @@
 exports.version = 1.0;
 exports.description = "Real-time monitoring for HFS-Monitor";
 exports.beforePlugin = "admin";
+exports.repo = "pavelnil/hfs-monitor-agent"
+exports.preview = ["https://github.com/pavelnil/HFS-Monitor/blob/main/screenshots/screenshot.jpg","https://github.com/pavelnil/HFS-Monitor/blob/main/screenshots/themesheet.jpg"]
 
 let lastTotalIn = 0;
 let lastTotalOut = 0;
